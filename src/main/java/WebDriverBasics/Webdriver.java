e8e25b20b686dacc22893c58ae7e136214d54bf6
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2024-05-12 15:13:55 +0530

    Adding selenium webdriber sessions

---
+++ src/main/java/WebDriverBasics/Webdriver.java
@@ -0,0 +1,54 @@
+package WebDriverBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Webdriver  {
+
+    public static void main(String[] args) {
+        //java + selenium code ( 4.x)
+        //open the browser
+
+        WebDriver driver = new ChromeDriver();
+
+        /**
+         * Entering the URL
+         */
+        driver.get("https://www.udemy.com/");
+        /**
+         * Gettign the Title
+         */
+        String title=driver.getTitle();
+        System.out.println("Page Title: " + title);
+        if (title.equals("Online Courses - Learn Anything, On Your Schedule | Udemy")){
+            System.out.println("title is correct");
+        }
+        else {
+            System.out.println("title is not correct");
+        }
+
+        /**
+         * Getting the URL
+         */
+        String url=driver.getCurrentUrl();
+        System.out.println(url);
+        if (url.contains("udemy.com")){
+            System.out.println("url is correct");
+        }
+        else {
+            System.out.println(" url is not correct ");
+        }
+
+        /**
+         * Closing the browser
+         */
+        //driver.close();
+        driver.quit();
+
+        /**
+         * Automation steps + validation = AUTOMATION TESTING
+         */
+
+
+    }
+}
