35995550871f41663cf1f0ebf6af14c125ea10c5
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2024-07-09 22:51:27 +0530

    Added Js alert operations , Js Alert , Js Confirm and Js promot

--- src/main/java/SeleniumConcepts/AlertHandling.java
+++ src/main/java/SeleniumConcepts/AlertHandling.java
@@ -18,6 +18,11 @@ public class AlertHandling {
 
         driver=new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.switchTo().alert();// NoAlertException: no such alert
+
+        /**J1.S Alert
+         *
+         */
         By jsAlert= By.xpath("//button[text()='Click for JS Alert']");
         driver.findElement(jsAlert).click();
         Thread.sleep(3000);
@@ -25,9 +30,29 @@ public class AlertHandling {
         String text= alert.getText();
         System.out.println(text);
 
-        alert.accept();
-        //alert.dismiss();
+        alert.accept();//ok button
+        alert.dismiss();
 
+        //2. JS Confirm
+        By jsConfirm= By.xpath("//button[text()='Click for JS Confirm']");
+        driver.findElement(jsConfirm).click();
+        Alert alert1 = driver.switchTo().alert();
+        System.out.println(alert1.getText());
+        //alert1.accept();//Ok
+        alert1.dismiss();//cancel
+
+
+        /**
+         * 3.JS Prompt
+         */
+        By jsPrompt= By.xpath("//button[text()='Click for JS Prompt']");
+        Thread.sleep(5000);
+        driver.findElement(jsPrompt).click();
+        Alert alert2 = driver.switchTo().alert();
+        System.out.println(alert2.getText());
+        alert2.sendKeys("Selenium automation");
+        alert1.accept();//Ok
+        //alert1.dismiss();//cancel
         driver.quit();
 
     }
